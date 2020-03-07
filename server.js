07007820bda8134d0add26ca079984de9c85272e
Antonio Alvarado <ant.36alv@gmail.com>
2020-03-06 20:09:04 -0500

    Get and delete methods have full functionality

--- server.js
+++ server.js
@@ -1,49 +1,75 @@
+//express
 const express = require("express")
 const app = express()
+//fs
+const fs = require("fs")
+const util = require("util")
+const fileReader = util.promisify(fs.readFile)
+const fileWriter = util.promisify(fs.writeFile)
+//file/directory paths
 const path = require("path")
-
 const OUTPUT_DIR = path.resolve(__dirname, "public")
-
-const db = path.join(path.resolve(__dirname, "db"), "db.json")
-
+const DB_DIR = path.resolve(__dirname, "db")
+const db = path.join(DB_DIR, "db.json")
+//port
 const PORT = process.env.PORT || 3000
-
-// const PORT = 3000
-
+//UUID
+const { uuid } = require("uuidv4");
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+//serve public folder as a static asset
+app.use(express.static("public"))
+//at the request of the notes page serve notes.html
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(OUTPUT_DIR, "notes.html"))
 })
 
-/*
-GET /api/notes - Should read the db.json file and return all saved notes as JSON.
+app.get("/api/notes", async (req, res) => {
 
-POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
-
-DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
-*/
-app.get("api/notes", (req, res) => {
-
-    return res.json(db)
+    try {
+        res.json(JSON.parse(await fileReader(db)))
+    } catch (err) {
+        if (err) throw err
+    }
 })
 
-app.post("api/notes", () => {
+app.post("/api/notes", async (req, res) => {
+
+    const note = req.body
+    note.id = uuid()
+
+    try {
+        const data = await fileReader(db)
+        const pData = JSON.parse(data)
+        pData.push(note)
+
+        fileWriter(db, JSON.stringify(pData))
+        res.json(JSON.parse(await fileReader(db)))
+    } catch (err) {
+        if (err) throw err//new Error("Issue adding to JSON")
+    }
+})
+
+
+app.delete("/api/notes/:id", async (req, res) => {
     
+    const id = req.params.id
+    try {
+        const data = await fileReader(db)
+        const pData = JSON.parse(data)
+        for(i = 0; i < pData.length; i++) {
+            if(id === pData[i].id) pData.splice(i, 1)
+        }
+        fileWriter(db, JSON.stringify(pData))
+        res.json(JSON.parse(await fileReader(db)))
+    } catch(err) {
+        if(err) throw err
+    }
 })
-// app.get()
-
-//bonus
-// app.edit()
-
-// app.delete()
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(OUTPUT_DIR, "index.html"))
 })
 
-app.listen(PORT, () => {
-    console.log("App listening on PORT " + PORT)
-})
+app.listen(PORT, () => console.log("App listening on PORT " + PORT))
